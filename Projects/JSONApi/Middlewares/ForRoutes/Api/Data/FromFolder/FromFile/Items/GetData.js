43eddd2d63ff5b51dfb0cd08d25e499c36c5ca78
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2022-10-15 12:36:44 +0530

    jatin

--- Projects/JSONApi/Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/GetData.js
+++ Projects/JSONApi/Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/GetData.js
@@ -1,9 +1,35 @@
-let dal = require("../../../../../../Dal/Api/Data/FromFolder/FromFile/Items/GetData");
+exports.AsArray = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                inFolderName: "",
+                inFileNameWithExtension: "",
+                inDataPK: ""
+            },
+            originalUrl: req.originalUrl
+        });
+    } else {
+        if (("KeshavSoft" in req) === false) {
+            res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+        } else {
+            if (("DataPk" in req.KeshavSoft) === false) {
+                res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
 
-exports.AsArray = async ({ inDataPk, inJsonConfig }) => {
-    return await dal.AsArray({ inDataPk, inJsonConfig });
-};
-
-exports.UsingFolderAndFile = async ({inFolderName, inFileNameWithExtension, inDataPK }) => {
-    return await dal.UsingFolderAndFile({ inFolderName, inFileNameWithExtension, inDataPK });
-};
+            } else {
+                if (("inFolderName" in req.body) === false) {
+                    res.json({ KTF: false, KReason: "inFolderName not found in body" })
+                } else {
+                    if (("inFileNameWithExtension" in req.body) === false) {
+                        console.log("eeeeeeeeee : ", "inFileNameWithExtension" in req.body);
+                        res.json({ KTF: false, KReason: "inFileNameWithExtension not found in body" });
+                        res.end();
+                    } else {
+                        next();
+                    };
+                };
+            };
+        };
+    };
+}; 
